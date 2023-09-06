e5a41be6f1dca8ddf78abc97f4f5e8515bd7461c
Stanislav Makovey <113332004+m3i9n@users.noreply.github.com>
2023-09-06 12:50:50 +0300

    Add files via upload
    SCRIP-2

--- canvas_game/game.js
+++ canvas_game/game.js
@@ -11,6 +11,8 @@ let game = {
     blocks: [],
     rows: 4,
     cols: 8,
+    width : 640,
+    height : 360,
     sprites: {
         background: null,
         ball: null,
@@ -72,6 +74,7 @@ let game = {
         });
     },
     render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
             this.ball.x, this.ball.y, this.ball.width, this.ball.height);
@@ -89,6 +92,9 @@ let game = {
             this.create();
             this.run();
         });
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 };
 
@@ -101,10 +107,14 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity;
+        this.dx = game.random(-this.velocity, this.velocity);
     },
     move() {
         if (this.dy) {
             this.y += this.dy;
+        }
+        if (this.dx) {
+            this.x += this.dx;
         }
     }
 };
@@ -124,7 +134,7 @@ game.platform = {
     start(direction) {
         if (direction === KEYS.LEFT) {
             this.dx = -this.velocity;
-        } else if (direcion === KEYS.RIGHT) {
+        } else if (direction === KEYS.RIGHT) {
             this.dx = this.velocity;
         }
     },
